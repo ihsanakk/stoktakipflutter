fd74b3c3d312435b83404d16e85f8ba4accc7d75
ihsanakk <aakkoyunlu.ihsan@gmail.com>
2023-03-12 14:05:20 +0300

    n0thing

--- lib/shared/enumLabel/label_names_enum.dart
+++ lib/shared/enumLabel/label_names_enum.dart
@@ -1,72 +1,70 @@
 class LabelNames {
-  static const String BOTTOM_NAVBAR_ITEM_1 = "Stok";
-  static const String BOTTOM_NAVBAR_ITEM_2 = "Satış";
-  static const String BOTTOM_NAVBAR_ITEM_3 = "Ürün";
-  static const String BOTTOM_NAVBAR_ITEM_4 = "Geçmiş";
-  static const List<String> INVENTORY_VIEW_APPBAR_TITLES = [
+  static String BOTTOM_NAVBAR_ITEM_1 = "Stok";
+  static String BOTTOM_NAVBAR_ITEM_2 = "Satış";
+  static String BOTTOM_NAVBAR_ITEM_3 = "Ürün";
+  static String BOTTOM_NAVBAR_ITEM_4 = "Geçmiş";
+  static List<String> INVENTORY_VIEW_APPBAR_TITLES = [
     "Stok Listesi",
     "Satış",
     "Ürün Detay",
     "Geçmiş"
   ];
 
-  static const String SERVICE_ERROR = "HATA";
+  static String SERVICE_ERROR = "HATA";
 
-  static const String LOGOUT = "Çıkış";
-  static const String WELCOME_SNACK = "Hoşgeldiniz";
-  static const String FAIL_LOGIN = "Giriş başarısız";
-  static const String FAIL_REGISTER = "Kayıt başarısız";
-  static const String SUCCESS_REGISTER = "Kayıt başarılı";
+  static String LOGOUT = "Çıkış";
+  static String WELCOME_SNACK = "Hoşgeldiniz";
+  static String FAIL_LOGIN = "Giriş başarısız";
+  static String FAIL_REGISTER = "Kayıt başarısız";
+  static String SUCCESS_REGISTER = "Kayıt başarılı";
 
-  static const String SALES_MODE_TOTAL_PRICE = "Genel Toplam";
-  static const String SALES_MODE_CLEAN_BUTTON = "Temizle";
-  static const String SALES_MODE_SCAN_BUTTON = "Tara";
-  static const String SALES_MODE_REMOVE_PRODUCT_SCAFFOLD_MESSAGE =
-      "Ürün çıkartıldı.";
+  static String SALES_MODE_TOTAL_PRICE = "Genel Toplam";
+  static String SALES_MODE_CLEAN_BUTTON = "Temizle";
+  static String SALES_MODE_SCAN_BUTTON = "Tara";
+  static String SALES_MODE_REMOVE_PRODUCT_SCAFFOLD_MESSAGE = "Ürün çıkartıldı.";
 
-  static const String PRODUCT_VIEW_DELETE = "Sil";
-  static const String PRODUCT_VIEW_UPDATE = "Güncelle";
-  static const String PRODUCT_VIEW_SCAN = "Tara";
+  static String PRODUCT_VIEW_DELETE = "Sil";
+  static String PRODUCT_VIEW_UPDATE = "Güncelle";
+  static String PRODUCT_VIEW_SCAN = "Tara";
 
-  static const String PRODUCT_DETAILS_PRODUCT_NAME = "Ürün Adı";
-  static const String PRODUCT_DETAILS_PRODUCT_BARCODE = "Barkod";
-  static const String PRODUCT_DETAILS_PRODUCT_CREATED_AT = "Oluşturulma Tarihi";
-  static const String PRODUCT_DETAILS_PRODUCT_DESCRIPTION = "Açıklama";
-  static const String PRODUCT_DETAILS_PRODUCT_PRICE = "Fiyat";
-  static const String PRODUCT_DETAILS_PRODUCT_QUANTITY = "Adet";
-  static const String PRODUCT_DETAILS_MESSAGE_PRODUCT_DELETED = "Ürün silindi";
-  static const String PRODUCT_DETAILS_MESSAGE_PRODUCT_SAVED = "Ürün kaydedildi";
-  static const String PRODUCT_DETAILS_MESSAGE_INVALID_PRODUCT_NAME =
+  static String PRODUCT_DETAILS_PRODUCT_NAME = "Ürün Adı";
+  static String PRODUCT_DETAILS_PRODUCT_BARCODE = "Barkod";
+  static String PRODUCT_DETAILS_PRODUCT_CREATED_AT = "Oluşturulma Tarihi";
+  static String PRODUCT_DETAILS_PRODUCT_DESCRIPTION = "Açıklama";
+  static String PRODUCT_DETAILS_PRODUCT_PRICE = "Fiyat";
+  static String PRODUCT_DETAILS_PRODUCT_QUANTITY = "Adet";
+  static String PRODUCT_DETAILS_MESSAGE_PRODUCT_DELETED = "Ürün silindi";
+  static String PRODUCT_DETAILS_MESSAGE_PRODUCT_SAVED = "Ürün kaydedildi";
+  static String PRODUCT_DETAILS_MESSAGE_INVALID_PRODUCT_NAME =
       "Ürün adı en az 3 haneli olmalıdır";
-  static const String PRODUCT_DETAILS_MESSAGE_INVALID_PRODUCT_BARCODE =
+  static String PRODUCT_DETAILS_MESSAGE_INVALID_PRODUCT_BARCODE =
       "Ürün barkodu en az 3 haneli olmalıdır";
 
-  static const String LOGIN_VIEW_MESSAGE_BAD_CREDENTIALS =
+  static String LOGIN_VIEW_MESSAGE_BAD_CREDENTIALS =
       "Şifre veya kullanıcı adı yanlış.";
-  static const String LOGIN_VIEW_MESSAGE_INVALID_FORM =
+  static String LOGIN_VIEW_MESSAGE_INVALID_FORM =
       "Lütfen hatalı alanları düzeltin.";
-  static const String LOGIN_VIEW_TITLE = "Giriş";
-  static const String LOGIN_VIEW_MESSAGE_EMAIL_REQUIRED =
+  static String LOGIN_VIEW_TITLE = "Giriş";
+  static String LOGIN_VIEW_MESSAGE_EMAIL_REQUIRED =
       "Email girilmesi zorunludur.";
-  static const String LOGIN_VIEW_MESSAGE_INVALID_EMAIL =
+  static String LOGIN_VIEW_MESSAGE_INVALID_EMAIL =
       "Lütfen geçerli bir email girin.";
-  static const String LOGIN_VIEW_MESSAGE_INVALID_PASSWORD =
+  static String LOGIN_VIEW_MESSAGE_INVALID_PASSWORD =
       "Lütfen geçerli bir şifre girin.";
-  static const String LOGIN_VIEW_MESSAGE_PASSWORD_REQUIRED =
+  static String LOGIN_VIEW_MESSAGE_PASSWORD_REQUIRED =
       "Şifre girilmesi zorunludur.";
-  static const String LOGIN_VIEW_LOGIN_BUTTON = "Giriş";
+  static String LOGIN_VIEW_LOGIN_BUTTON = "Giriş";
 
-  static const String LOGIN_VIEW_LABEL_EMAIL = "Email";
+  static String LOGIN_VIEW_LABEL_EMAIL = "Email";
 
-  static const String LOGIN_VIEW_LABEL_PASSWORD = "Şifre";
+  static String LOGIN_VIEW_LABEL_PASSWORD = "Şifre";
 
-  static const String INVENTORY_VIEW_SEARCH_HINT = "Ara";
+  static String INVENTORY_VIEW_SEARCH_HINT = "Ara";
 
-  static const String REGISTER_VIEW_MESSAGE_INVALID_PASSWORD =
-      "Şifreler eşleşmiyor";
-  static const String REGISTER_VIEW_LABEL_PASSWORD_CONFIRM = "Şifre Tekrar";
-  static const String REGISTER_VIEW_TITLE = "Kayıt ol";
-  static const String REGISTER_BUTTON_REGISTER = "Kayıt ol";
-  static const String REGISTER_HAVE_ACCOUNT_TEXT = "Hesabınız yok mu?";
+  static String REGISTER_VIEW_MESSAGE_INVALID_PASSWORD = "Şifreler eşleşmiyor";
+  static String REGISTER_VIEW_LABEL_PASSWORD_CONFIRM = "Şifre Tekrar";
+  static String REGISTER_VIEW_TITLE = "Kayıt ol";
+  static String REGISTER_BUTTON_REGISTER = "Kayıt ol";
+  static String REGISTER_HAVE_ACCOUNT_TEXT = "Hesabınız yok mu?";
   // TODO -> refactor for languages settings enum or something else search for it
 }
